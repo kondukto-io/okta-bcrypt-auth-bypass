2defc02d9d7046c26f863c169c764f636510dee2
Cenk Kalpakoğlu <cenk@kondukto.io>
2024-11-04 18:53:12 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10; 
+
+// This is how bcrypt has been used:
+// bcrypt(userid + username + password()
+
+// we don't know how the userId's are generated, so use UUIDv4
+var userid = "b91fa9b4-69f1-4779-8d45-73f8653057f3"; 
+
+// very long username
+var username = "my.very.long.username.with.more.characters@kondukto.io" // 54 bytes long
+
+// valid random password 
+var password = "randomStrongPassword"
+var validInput = userid + username + password;
+
+// simulate bypass input -- can be anything
+var password2 = "AAAAAAAAAAAAAAAAAAA"
+var bypassInput = userid + username + password;
+
+bcrypt.genSalt(saltRounds, function(err, salt) {
+	bcrypt.hash(validInput, salt, function(err, hash) {
+		console.log(hash);
+	});
+
+	bcrypt.hash(bypassInput, salt, function(err, hash) {
+		console.log(hash);
+	});
+});
